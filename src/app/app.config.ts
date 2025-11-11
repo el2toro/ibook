322be24acb987d79780372368ae87b64e3f1384d
el2toro <vasea.badragan@gmail.com>
2025-11-11 08:13:24 +0100

    add styles and primeicons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
@@ -10,6 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideAnimations(),
     providePrimeNG({
             theme: {
                 preset: Aura
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AppointmentWidgetComponent } from "./widgets/appointment-widget/appointment-widget.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AppointmentWidgetComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
