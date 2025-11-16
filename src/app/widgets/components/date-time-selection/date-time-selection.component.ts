442446013c37ebb6d564855b1b3f5a5cf080e8a4
el2toro <vasea.badragan@gmail.com>
2025-11-16 15:50:47 +0100

    add date picker

--- src/app/widgets/components/date-time-selection/date-time-selection.component.ts
+++ src/app/widgets/components/date-time-selection/date-time-selection.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DatePickerModule } from 'primeng/datepicker';
 
 @Component({
   selector: 'app-date-time-selection',
   templateUrl: './date-time-selection.component.html',
-  styleUrls: ['./date-time-selection.component.scss']
+  styleUrls: ['./date-time-selection.component.scss'],
+   imports: [FormsModule, DatePickerModule]
 })
 export class DateTimeSelectionComponent implements OnInit {
+  @Output() selectDateEvent = new EventEmitter();
+  date: Date[] | undefined;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onDateSelect(){
+    this.selectDateEvent.emit(this.date)
+  }
 }
