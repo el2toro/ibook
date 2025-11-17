5769e438eebf3f779193e8e3284e08990c84411a
el2toro <vasea.badragan@gmail.com>
2025-11-17 18:44:44 +0100

    add guest guard

---
+++ src/app/core/guards/guest-guard.ts
@@ -0,0 +1,19 @@
+// guest.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GuestGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/dashboard']); // redirect logged-in users
+      return false;
+    }
+    return true; // allow guests
+  }
+}
