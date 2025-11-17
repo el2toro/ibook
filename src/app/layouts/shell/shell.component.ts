059dffe26ca9dcb70165024b24355834bbde3d45
el2toro <vasea.badragan@gmail.com>
2025-11-17 18:46:12 +0100

    move components

--- src/app/layouts/shell/shell.component.ts
+++ src/app/layouts/shell/shell.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.scss']
+  styleUrls: ['./shell.component.scss'],
+  imports: [RouterOutlet]
 })
 export class ShellComponent implements OnInit {
 
