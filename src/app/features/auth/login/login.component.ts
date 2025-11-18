19cc395ae8bd73c59555a23e53fa10f488c86052
el2toro <vasea.badragan@gmail.com>
2025-11-18 11:26:17 +0100

    implement login page

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  imports: [IconFieldModule, InputTextModule, InputIconModule, ReactiveFormsModule, ButtonModule, CheckboxModule]
 })
 export class LoginComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  formGroup!: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  buildForm() {
+    this.formGroup = this.formBuilder.group({
+      email: [null],
+      password: [null],
+      rememberMe: [false]
+    });
+
+    this.formGroup.valueChanges.subscribe(value => {
+      console.log(value);
+    });
+  }
 }
