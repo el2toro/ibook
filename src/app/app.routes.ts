45a16d30bf25dab006a1f301fef0247893d93177
el2toro <vasea.badragan@gmail.com>
2026-05-16 13:22:46 +0200

    add appointment route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { AuthGuard } from './core/guards/auth-guard';
 import { DashboardCardComponent } from './features/dashboard/components/dashboard-card/dashboard-card.component';
 import { SignupComponent } from './features/auth/signup/signup.component';
 import { GuestGuard } from './core/guards/guest-guard';
+import { AppointmentWidgetComponent } from './widgets/appointment-widget/appointment-widget.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
   { path: 'signup', component: SignupComponent, canActivate: [GuestGuard] },
+  { path: 'appointment', component: AppointmentWidgetComponent},
   {
     path: 'dashboard',
     component: DashboardCardComponent,
